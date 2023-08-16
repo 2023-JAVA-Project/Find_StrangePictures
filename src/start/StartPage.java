ee5b2959cf9edc5d97d309e9aa0d693cfb541adf
ezi-s-u <ljsgoo0617@gmail.com>
2023-08-16 18:29:55 +0900

    [Feat] start package 생성

---
+++ src/start/StartPage.java
@@ -0,0 +1,36 @@
+package start;
+
+import java.awt.Cursor;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class StartPage extends JPanel {
+	
+	Image img = new ImageIcon(StartPage.class.getResource("../img/startpageimage.png")).getImage();
+			
+	public void paintComponent(Graphics g) {
+		g.drawImage(img, 0, 0, null);
+		setOpaque(false);
+	}
+	
+	StartButton startBtn;
+	ExplainButton explainBtn;
+	
+	public StartPage() {
+		setLayout(null); // 기본설정인 FlowLayout를 비활성화함
+		startBtn = new StartButton();
+		startBtn.setVisible(true);
+		startBtn.setBounds(190, 486, 348, 105);
+		
+		explainBtn = new ExplainButton();
+		explainBtn.setBounds(642, 486, 367, 105);
+		
+		add(startBtn);
+		add(explainBtn);
+	}
+}
