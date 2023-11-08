30c71d244cef55a24b351991ef1668e7907ceea9
ezi-s-u <ljsgoo0617@gmail.com>
2023-11-08 12:47:37 +0900

    [Refactor] 게임 페이지 투명라벨 나타나게, 클릭 이벤트

--- src/game/GamePage.java
+++ src/game/GamePage.java
@@ -13,15 +13,17 @@ public class GamePage extends JPanel{
 		setOpaque(false);
 	}
 	int Num=1;
-	int Count=5;
+	static int Count=5;
 	Timer timerBar;
 	Thread threadBar;
 	int second=30;
 
-	int[][] labelLoc = {{0, 3, 55, 612, 100, 23, 280, 70, 460, 130},{1,2,3,4,5,6,7,8,9,10},{0, 3, 55, 612, 100, 23, 280, 70, 460, 130},{0, 3, 55, 612, 100, 23, 280, 70, 460, 130},{0, 3, 55, 612, 100, 23, 280, 70, 460, 130},{0, 3, 55, 612, 100, 23, 280, 70, 460, 130},{0, 3, 55, 612, 100, 23, 280, 70, 460, 130},{0, 3, 55, 612, 100, 23, 280, 70, 460, 130}}; // 두번째 이미지부터 넣어야함 (7개만)
+	static JLabel count_label;
+
+	int[][] labelLoc = {{0, 3, 55, 60, 100, 23, 280, 70, 460, 130},{1,2,3,4,5,6,7,8,9,10},{0, 3, 55, 612, 100, 23, 280, 70, 460, 130},{0, 3, 55, 612, 100, 23, 280, 70, 460, 130},{0, 3, 55, 612, 100, 23, 280, 70, 460, 130},{0, 3, 55, 612, 100, 23, 280, 70, 460, 130},{0, 3, 55, 612, 100, 23, 280, 70, 460, 130},{0, 3, 55, 612, 100, 23, 280, 70, 460, 130}}; // 두번째 이미지부터 넣어야함 (7개만)
 
 	public GamePage() {
-		JLabel count_label=new JLabel(" "+Count);
+		count_label=new JLabel(" "+Count);
 		JLabel number=new JLabel("No."+Num);
 
 		count_label.setFont(new Font("Arial",Font.BOLD ,30));
@@ -37,7 +39,7 @@ public class GamePage extends JPanel{
 		threadBar.start();
 
 		// 틀린그림 이미지 Panel
-		GameImagePanel gameImg = new GameImagePanel();
+		GameImagePanel gameImg = new GameImagePanel(labelLoc[0]);
 		add(count_label);
 		add(number);
 		add(gameImg);
@@ -56,18 +58,32 @@ public class GamePage extends JPanel{
 
 
 		// 시간 다 되기 전까지
-		while(timerBar.getWidth() > 0) {
-//			gameImg.touchLabel[i]=jk;
-			if(Count == 0) {
-				gameImg.uploadImage();
-				Count = 5; // 다시 5로 초기화
-			}
+//		while(timerBar.getWidth() > 0) {
+////			gameImg.touchLabel[i]=jk;
+//			if(Count == 0) {
+//				gameImg.uploadImage();
+//				Count = 5; // 다시 5로 초기화
+//			}
+//
+//			// 투명 label 다시 보이게 하기
+//			for (int j = 0; j < gameImg.touchLabel.length; j++){ // 투명 label array 5개
+//				gameImg.replaceTouchLabel(labelLoc[j]);
+//				gameImg.touchLabel[j].setVisible(true);
+//			}
+//		}
 
-			// 투명 label 다시 보이게 하기
-			for (int j = 0; j < gameImg.touchLabel.length; j++){ // 투명 label array 5개
-				gameImg.replaceTouchLabel(labelLoc[j]);
+
+		if(Count == 0) {
+			gameImg.uploadImage();
+			for (int j = 0; j < gameImg.touchLabel.length - 1; j++){ // 투명 label array 5개
+				gameImg.replaceTouchLabel(labelLoc[j+1]);
 				gameImg.touchLabel[j].setVisible(true);
 			}
+			Count = 5; // 다시 5로 초기화
 		}
+	}
+
+	static public void updateCountLabel() {
+		count_label.setText(" " + Count);
 	}
 }
